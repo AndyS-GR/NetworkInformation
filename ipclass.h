03adc79631aff028f0606c0aa7d144c1cbb74f76
AndyS-GR <u20201b763@upc.edu.pe>
2023-04-20 18:55:20 -0500

    first commit

---
+++ ipclass.h
@@ -0,0 +1,27 @@
+#ifndef _IPCLASS_H_
+#define _IPCLASS_H_
+
+#include <iostream>
+
+using namespace std;
+
+class IpClass {
+    private: int* IP = new int[4];
+    private: int* MASK = new int[4];
+    
+    IpClass(int current_ip[4], int current_mask[4]){
+        for (int i = 0; i < 4; i++){
+            this->IP[i] = current_ip[i];
+            this->MASK[i] = current_mask[i];
+        }
+    }
+
+    void getIpNetwork(){
+        cout << "hello work" << endl;
+    }
+
+    
+};
+
+
+#endif
